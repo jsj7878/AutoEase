988ff50d351b2e9c7991e7596a0c31ee79b59b77
jsj7878 <sejin8603@naver.com>
2024-12-06 12:10:28 +0900

    feat: 차 detail 화면 작업중

--- app/src/main/java/com/yj/autoease/fragments/CarDetailFragment.kt
+++ app/src/main/java/com/yj/autoease/fragments/CarDetailFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageButton
+import android.widget.Toast
 import com.yj.autoease.R
 
 class CarDetailFragment : Fragment() {
@@ -15,7 +17,53 @@ class CarDetailFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_car_detail, container, false)
+        val view = inflater.inflate(R.layout.fragment_car_detail, container, false)
+        val buttonArray = arrayOf(
+            view.findViewById<ImageButton>(R.id.lockButton),
+            view.findViewById<ImageButton>(R.id.blueButton),
+            view.findViewById<ImageButton>(R.id.clarkButton),
+            view.findViewById<ImageButton>(R.id.emerButton),
+            view.findViewById<ImageButton>(R.id.heaterButton),
+            view.findViewById<ImageButton>(R.id.powerButton)
+        )
+        for (button in buttonArray){
+                button.setOnClickListener {
+                    button.isSelected = !button.isSelected
+                    when(button.id){
+                        R.id.lockButton ->{
+                                button.setImageResource(if (button.isSelected) R.drawable.ic_lock else R.drawable.ic_lockopen)
+                        }
+                        R.id.powerButton ->{
+
+                        }
+                        R.id.clarkButton ->{
+
+                        }
+                        R.id.blueButton ->{
+
+                        }
+                        R.id.heaterButton ->{
+
+                        }
+                        R.id.emerButton ->{
+
+                        }
+                }
+
+
+            }
+        }
+        //            if(button.id == R.id.lockButton){
+//                button.setOnClickListener {
+//                    button.isSelected = !button.isSelected
+//                }
+//                button.setImageResource(if (button.isSelected) R.drawable.ic_lock else R.drawable.ic_lockopen)
+//            } else {
+//                button.setOnClickListener {
+//                    button.isSelected = !button.isSelected
+//                }
+//            }
+        return view
     }
 
 }
